4499925ec9d394e49ff1d9e6f3022449b280828f
tutanck <tutanck@gmail.com>
2017-07-22 16:30:58 +0200

    profile page almost done

--- ProfileTableViewController.swift
+++ ProfileTableViewController.swift
@@ -8,50 +8,112 @@
 
 import UIKit
 
-class ProfileTableViewController: UITableViewController {
+class ProfileTableViewController: UITableViewController, UITextFieldDelegate, UITextViewDelegate {
     
     
     @IBOutlet weak var photoImageView: UIImageView!
     
-    @IBOutlet weak var typeSegmentedControl: UISegmentedControl!
     
-    @IBOutlet weak var usernameLabel: UITextField!
+    //SegmentedControl
+
+    @IBOutlet weak var typeSegmentedControl: UISegmentedControl!
+    @IBAction func typeSegmentedControlChanged(_ sender: UISegmentedControl) { showRightBarButtonItem() }
+    
+    
+    //TextField
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()         // Hide the keyboard
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) { showRightBarButtonItem() }
+    
+    
+    //TextView
     
     @IBOutlet weak var interestTextView: UITextView!
 
+    
+    func textViewShouldReturn(_ textView: UITextView) -> Bool {
+        textView.resignFirstResponder()         // Hide the keyboard
+        return true
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) { showRightBarButtonItem() }
+    
+    
+    
+    //Switchs
+    
     @IBOutlet weak var unqualifiedSwitch: UISwitch!
-    
+    @IBAction func unqualifiedSwitchChanged(_ sender: UISwitch) { showRightBarButtonItem() }
+
     @IBOutlet weak var partTimeSwitch: UISwitch!
+    @IBAction func partTimeSwitchChanged(_ sender: UISwitch) { showRightBarButtonItem() }
     
     
     
+    
+    // MARK: - System Events
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        begin()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
     
+
     
     
     /*
-    // MARK: - Navigation
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    
+    
+    
+    // MARK: - private methods
+    
+    private func begin(){
+        hideRightBarButtonItem()
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        //photoImageView
+        
+        usernameTextField.delegate = self
+
+        //typeSegmentedControl
+        
+        interestTextView.delegate = self
+        
+        //unqualifiedSwitch
+        
+        //partTimeSwitch
     }
-    */
-
+    
+    private func hideRightBarButtonItem(){
+        if let rightBarButton = self.navigationItem.rightBarButtonItem {
+            rightBarButton.isEnabled = false
+        }
+    }
+    
+    private func showRightBarButtonItem(){
+        if let rightBarButton = self.navigationItem.rightBarButtonItem {
+            rightBarButton.isEnabled = true
+        }
+    }
+    
 }
