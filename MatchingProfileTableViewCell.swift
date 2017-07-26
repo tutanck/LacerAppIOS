b52cfb6dbba5804b5ac766f465eedee362981871
tutanck <tutanck@gmail.com>
2017-07-27 00:46:14 +0200

    jpp

---
+++ MatchingProfileTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MatchingProfileTableViewCell.swift
+//  LacerApp
+//
+//  Created by Joan Angb on 26/07/2017.
+//  Copyright © 2017 DevArtisant. All rights reserved.
+//
+
+import UIKit
+
+class MatchingProfileTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
