b3d052f7e0f7d0d08e4f2dcb4bb25efeac445c71
tutanck <tutanck@gmail.com>
2017-08-23 22:57:05 +0200

    Messenger version 2 : still basick but with blue

--- LacerApp/MessengerCollectionViewController.swift
+++ LacerApp/MessengerCollectionViewController.swift
@@ -21,17 +21,17 @@ class MessengerCollectionViewController : UICollectionViewController, UICollecti
     }
     
     var messages: [Message] = [
+        Message(text : "Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla ", author : nil),
+        Message(text : "Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla ", author : nil),
+        Message(text : "Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla ", author : nil),
+        Message(text : "Yes, totally looking to buy an iPhone 7.", author : nil),
+        Message(text : "Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla ", author : nil),
+        Message(text : "Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla ", author : nil),
         Message(text : "Bla Bla Bla", author : nil),
         Message(text : "Bla Bla Bla", author : nil),
+        Message(text : "Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla ", author : nil),
         Message(text : "Bla Bla Bla", author : nil),
-        Message(text : "Bla Bla Bla", author : nil),
-        Message(text : "Bla Bla Bla", author : nil),
-        Message(text : "Bla Bla Bla", author : nil),
-        Message(text : "Bla Bla Bla", author : nil),
-        Message(text : "Bla Bla Bla", author : nil),
-        Message(text : "Bla Bla Bla", author : nil),
-        Message(text : "Bla Bla Bla", author : nil),
-        Message(text : "Bla Bla Bla", author : nil)
+        Message(text : "Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla", author : nil)
     ]
     
     override func viewDidLoad() {
@@ -57,19 +57,73 @@ class MessengerCollectionViewController : UICollectionViewController, UICollecti
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! MessageCollectionViewCell
         
-        cell.messageTextView.text = messages[indexPath.item].text
         
         let messageText = messages[indexPath.item].text
         
-        cell.profileImageView.image = UIImage(named: "userPhoto")
         
-        let size = CGSize(width: 250, height: 1000)
-        let options = NSStringDrawingOptions.usesFontLeading.union(.usesLineFragmentOrigin)
-        let estimatedFrame = NSString(string: messageText).boundingRect(with: size, options: options, attributes: [NSFontAttributeName: UIFont.systemFont(ofSize: 18)], context: nil)
         
-        cell.messageTextView.frame = CGRect(x: 48 + 8, y: 0, width: estimatedFrame.width + 16, height: estimatedFrame.height + 20)
+        let estimatedFrame = NSString(
+            string: messageText
+            ).boundingRect(
+                with: CGSize(
+                    width: 250/*width we want for the messageTextView rect*/,
+                    height: 1000/*arbitrary height to have enougth space for text in the messageTextView rect (for making estimations of the real height needed)*/
+                ),
+                options: NSStringDrawingOptions.usesFontLeading.union(.usesLineFragmentOrigin),
+                attributes: [NSFontAttributeName: UIFont.systemFont(ofSize: 18/*textFontSize*/)],
+                context: nil
+        )
         
-        cell.textBubbleView.frame = CGRect(x: 48, y: 0, width: estimatedFrame.width + 16 + 8, height: estimatedFrame.height + 20)
+        
+        
+        
+        cell.messageTextView.text = messageText
+        
+        
+        if  indexPath.item % 2 == 0{
+            
+            
+            cell.profileImageView.image = UIImage(named: "userPhoto")
+            
+            cell.messageTextView.frame = CGRect(
+                x: 48/*natural messageTextView's margin to the frame*/ + 8 /*text's margin to the left by shifting the messageTextView's position by 8 pixels from the textBubbleView */,
+                y: 0,
+                width: estimatedFrame.width + 16 /*prevent text overflow*/,
+                height: estimatedFrame.height + 20/*correction*/
+            )
+            
+            cell.textBubbleView.frame = CGRect(
+                x: 48/*natural textBubbleView's margin to the frame*/ ,
+                y: 0,
+                width: estimatedFrame.width + 16/*prevent text overflow*/  + 8/*text's margin to the right by extending by 8 pixels the textBubbleView's width */,
+                height: estimatedFrame.height + 20/*correction*/
+            )
+            cell.profileImageView.isHidden = false
+            cell.textBubbleView.backgroundColor = UIColor(white: 0.95, alpha: 1)
+            cell.messageTextView.textColor = UIColor.black
+            
+        } else {
+            
+            
+            //outgoing sending message
+            
+            cell.messageTextView.frame = CGRect(
+                x: view.frame.width - estimatedFrame.width - 16 - 16 /*prevent text overflow*/,
+                y: 0,
+                width: estimatedFrame.width + 16 /*prevent text overflow*/,
+                height: estimatedFrame.height + 20/*correction*/
+            )
+            
+            cell.textBubbleView.frame = CGRect(
+                x: view.frame.width - estimatedFrame.width - 16 - 8 - 16 /*prevent text overflow*/,
+                y: 0,
+                width: estimatedFrame.width + 16 /*prevent text overflow*/ + 8,
+                height: estimatedFrame.height + 20/*correction*/
+            )
+            cell.profileImageView.isHidden = true
+            cell.textBubbleView.backgroundColor = UIColor(red: 0, green: 137/255, blue: 249/255, alpha: 1)
+            cell.messageTextView.textColor = UIColor.white
+        }
         
         return cell
     }
@@ -79,13 +133,16 @@ class MessengerCollectionViewController : UICollectionViewController, UICollecti
         let estimatedFrame = NSString(
             string: messages[indexPath.item].text
             ).boundingRect(
-                with: CGSize(width: 250, height: 1000),
+                with: CGSize(
+                    width: 250/*width we want for the messageTextView rect*/,
+                    height: 1000/*arbitrary height to have enougth space for text in the messageTextView rect (for making estimations of the real height needed)*/
+                ),
                 options: NSStringDrawingOptions.usesFontLeading.union(.usesLineFragmentOrigin),
-                attributes: [NSFontAttributeName: UIFont.systemFont(ofSize: 18)],
+                attributes: [NSFontAttributeName: UIFont.systemFont(ofSize: 18/*textFontSize*/)],
                 context: nil
         )
         
-        return CGSize(width: view.frame.width, height: estimatedFrame.height + 20)
+        return CGSize(width: view.frame.width, height: estimatedFrame.height + 20/*correction*/)
     }
     
     
